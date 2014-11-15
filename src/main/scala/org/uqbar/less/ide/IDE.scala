65eb7a58155e9b23a57a660d686fb0719b97ddb2
Nicolas <Nicolas Scarcella>
2014-11-15 19:27:53 -0300

    Refactoring toolbar and adding menu

--- src/main/scala/org/uqbar/less/ide/IDE.scala
+++ src/main/scala/org/uqbar/less/ide/IDE.scala
@@ -1,77 +1,49 @@
 package org.uqbar.less.ide
 
-import java.awt.Color
-import java.awt.Color._
-import java.awt.Font
-import java.awt.font.FontRenderContext
-import java.awt.geom.AffineTransform
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
-import java.text.SimpleDateFormat
-import java.util.Date
-import scala.collection.mutable.StringBuilder
 import scala.language.reflectiveCalls
 import scala.swing.Action
-import scala.swing.Alignment
 import scala.swing.BorderPanel
 import scala.swing.BorderPanel.Position.Center
 import scala.swing.BorderPanel.Position.East
 import scala.swing.BorderPanel.Position.North
 import scala.swing.BorderPanel.Position.South
-import scala.swing.BorderPanel.Position.West
-import scala.swing.BoxPanel
-import scala.swing.Button
-import scala.swing.CheckBox
 import scala.swing.Component
-import scala.swing.Dialog
 import scala.swing.Dimension
 import scala.swing.FileChooser
-import scala.swing.FlowPanel
 import scala.swing.Insets
 import scala.swing.Label
-import scala.swing.ListView.IntervalMode.Single
 import scala.swing.MainFrame
 import scala.swing.MenuItem
 import scala.swing.Orientation.Horizontal
-import scala.swing.Orientation.Vertical
 import scala.swing.ScrollPane
 import scala.swing.SequentialContainer
 import scala.swing.SimpleSwingApplication
-import scala.swing.Slider
 import scala.swing.SplitPane
-import scala.swing.TextComponent
-import scala.swing.TextField
-import scala.swing.event.ButtonClicked
-import scala.swing.event.KeyTyped
-import scala.swing.event.SelectionChanged
-import scala.swing.event.ValueChanged
 import org.uqbar.less.Bytecode._
 import org.uqbar.less.SemanticModel.Sentence
 import org.uqbar.less.SemanticModel._
 import org.uqbar.less.encoder.Encode
+import org.uqbar.less.encoder.PreferenceFixture
 import org.uqbar.less.eval.Eval
-import org.uqbar.less.eval.State
+import org.uqbar.less.ide.components.ConfigModal
+import org.uqbar.less.ide.components.Console
+import org.uqbar.less.ide.components.Editor
+import org.uqbar.less.ide.components.EditorParsed
 import org.uqbar.less.obj.eval.Compile
-import org.uqbar.less.parser.Parse
-import org.uqbar.less.encoder._
 import javax.swing.ImageIcon
 import javax.swing.JToolBar
 import javax.swing.KeyStroke.getKeyStroke
-import javax.swing.text.{ TabSet => JTabSet }
-import javax.swing.text.TabStop
-import scala.swing.ComboBox
-import scala.swing.ListView
-import org.uqbar.less.ide.components._
-import org.uqbar.less.ide.components.StyleAttribute._
-import java.awt.FontMetrics
-import org.uqbar.less.encoder.StringPreference
-import org.uqbar.less.encoder.PreferenceFixture
-import org.uqbar.less.encoder.EncodingPreference
-import org.uqbar.less.encoder.IntPreference
-import org.uqbar.less.encoder.BooleanPreference
+import scala.swing.Menu
+import scala.swing.MenuBar
+import scala.swing.FlowPanel
+import scala.swing.event.Key
+import scala.swing.Button
+import scala.swing.SequentialContainer.Wrapper
 
 object LessIDE extends SimpleSwingApplication {
 
@@ -84,14 +56,30 @@ object LessIDE extends SimpleSwingApplication {
 
 		//─────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 
-		val runAction = menuButton("Run", "ctrl P"){ runInConsole }
-		val refreshAction = menuButton("Refresh", "ctrl R"){ refresh }
-		val saveAction = menuButton("Save", "ctrl S") { saveToFile }
-		val openAction = menuButton("Open", "ctrl O"){ openFile }
-		val configAction = menuButton("Config", "ctrl F"){ config }
+		val saveAction = action("Save", "ctrl S") { saveToFile }
+		val openAction = action("Open", "ctrl O"){ openFile }
+		val configAction = action("Config", "ctrl F"){ config }
+		val runAction = action("Run", "ctrl P"){ runInConsole }
+		val refreshAction = action("Refresh", "ctrl R"){ refresh }
 
 		val console = new Console
 		val editor = new Editor
+
+		menuBar = menubar(
+			"File" -> saveAction,
+			"File" -> openAction,
+			"File" -> refreshAction,
+			"Options.Preferences" -> configAction,
+			"Run" -> runAction
+		)
+
+		val mainToolBar = toolBar(
+			openAction,
+			saveAction,
+			refreshAction,
+			runAction,
+			configAction
+		)
 
 		val statusBar = new BorderPanel {
 			protected val status = new Label
@@ -111,14 +99,46 @@ object LessIDE extends SimpleSwingApplication {
 		}
 
 		//─────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+		// GUI BUILD
+		//─────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+
+		protected def action(name: String, accelerator: String = "")(body: => Unit) = {
+			val action = Action(name)(body)
+			Option(getClass.getResource(s"/icons/$name.gif")).map(image => action.icon = new ImageIcon(image))
+			action.accelerator = Option(getKeyStroke(accelerator))
+			action
+		}
+
+		protected def toolBar(actions: Action*) = new Component with SequentialContainer.Wrapper {
+			override lazy val peer: JToolBar = new JToolBar
+			for (action <- actions) peer.add(new Button(action) { text = "" }.peer)
+		}
+
+		protected def menubar(entries: (String, Action)*) = new MenuBar {
+
+			for ((path, action) <- entries) {
+				def subMenu(name: String, target: Wrapper = this) = target.contents.collectFirst{ case m: Menu if m.text == name => m }
+				def getOrCreateMenu(path: Seq[String], target: Wrapper = this): Wrapper = {
+					path.headOption.fold(target){ nextName =>
+						val next = subMenu(nextName, target).getOrElse{
+							val menu = new Menu(nextName)
+							target.contents += menu
+							menu
+						}
+
+						getOrCreateMenu(path.tail, next)
+					}
+				}
+
+				getOrCreateMenu(path.split('.')).contents += new MenuItem(action)
+			}
+		}
+
+		//─────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 
 		contents = new BorderPanel {
-			add(toolBar(
-				openAction,
-				saveAction,
-				refreshAction,
-				runAction,
-				configAction
+			add(new FlowPanel(
+				mainToolBar
 			), North)
 
 			add(new SplitPane(Horizontal, new ScrollPane(editor), new ScrollPane(console)), Center)
@@ -141,23 +161,6 @@ object LessIDE extends SimpleSwingApplication {
 		title = "Less IDE"
 		size = new Dimension(800, 600)
 		centerOnScreen
-
-		//─────────────────────────────────────────────────────────────────────────────────────────────────────────────────
-		// GUI BUILD
-		//─────────────────────────────────────────────────────────────────────────────────────────────────────────────────
-
-		protected def menuButton(itemName: String, keystroke: String = "")(action: => Unit) = new MenuItem(Action("")(action)) {
-			Option(getClass.getResource(s"/icons/$itemName.gif")).fold{ action.title = name }{ r => icon = new ImageIcon(r) }
-			action.accelerator = Option(getKeyStroke(keystroke))
-			margin = new Insets(0, 0, 0, 20)
-		}
-
-		protected def toolBar(components: Component*) = new Component with SequentialContainer.Wrapper {
-			override lazy val peer: JToolBar = new JToolBar
-			for (component <- components) peer.add(component.peer)
-			maximumSize = new Dimension(34 * components.size, 30)
-			preferredSize = maximumSize
-		}
 
 		//─────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 		// ACTIONS
